dc2e45567176a5790a776d184eee28e110101ace
OSora0302 <63058025+OSora0302@users.noreply.github.com>
2020-11-14 21:33:22 +0900

    Implement identity function and softmax function

--- playground/activation_functions.py
+++ playground/activation_functions.py
@@ -10,6 +10,15 @@ def sigmoid(x):
 def relu(x):
     return np.maximum(x, 0)
 
+def identity_function(x):
+    return x
+
+def softmax(x):
+    c = np.max(x)
+    exp_x = np.exp(x - c)
+    sum_exp_x = np.sum(exp_x)
+    return exp_x / sum_exp_x
+
 def check(f):
     x = np.arange(-5.0, 5.0, 0.1)
     y = f(x)
