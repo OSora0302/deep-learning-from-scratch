1490e598d070bc62d63a505359cda94f517ecf1b
OSora0302 <63058025+OSora0302@users.noreply.github.com>
2020-11-21 20:41:39 +0900

    Improve softmax to prevent overflow

--- playground/activation_functions.py
+++ playground/activation_functions.py
@@ -14,10 +14,15 @@ def identity_function(x):
     return x
 
 def softmax(x):
+    """
     c = np.max(x)
     exp_x = np.exp(x - c)
     sum_exp_x = np.sum(exp_x)
     return exp_x / sum_exp_x
+    """
+    # 誤差を防ぐ
+    x = x - np.max(x, axis=-1, keepdims=True)
+    return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
 
 def check(f):
     x = np.arange(-5.0, 5.0, 0.1)
--- playground/loss_function.py
+++ playground/loss_function.py
@@ -6,7 +6,7 @@ def sum_squared_error(y, t):
 def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
-        y = y.reshape(1, t.size)
+        y = y.reshape(1, y.size)
 
     if t.size == y.size:
         t = t.argmax(axis=1)
